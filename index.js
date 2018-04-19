b469478b25d640514462e3382ec8a4c1e6d7544c
Justin Tooley <jstooley823@gmail.com>
2018-04-19 22:31:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ function loadIssue() {
 }
 function loadIssue() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0;i<issues.length;i++) {
-    var result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
 }
